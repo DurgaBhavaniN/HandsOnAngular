80a51e7c6605510a6d3e764985f65de9e27eed5a
DurgaBhavaniN <durgabhavanid347@gmail.com>
2020-02-11 15:21:54 -0800

    11-2-2020

--- Angular-Forms/src/app/login/login.component.ts
+++ Angular-Forms/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder,FormGroup,Validators} from "@angular/forms";
+import { User } from '../user';
 
 
 @Component({
@@ -10,8 +11,11 @@ import {FormBuilder,FormGroup,Validators} from "@angular/forms";
 export class LoginComponent implements OnInit {
 loginForm:FormGroup;
 submitted=false;
+item:User;
   constructor(private formbuilder:FormBuilder) 
-  {}
+  {
+    this.item=new User();
+  }
 
   ngOnInit() {
     this.loginForm=this.formbuilder.group({
@@ -22,13 +26,22 @@ submitted=false;
 onSubmit()
 {
 this.submitted=true;
-if(!this.loginForm.invalid)
+if(this.loginForm.invalid)
 {
   return;
 }
 else
 {
 alert("Form is Validated");
+console.log(this.loginForm.value)//return java script object
+console.log(JSON.stringify(this.loginForm.value))//return json object
+console.log(this.loginForm.value["uname"])
+console.log(this.loginForm.value["pwd"])
+//assign values to model
+this.item.uname=this.loginForm.value["uname"]
+this.item.pwd=this.loginForm.value["pwd"]
+console.log(this.item);
+//this.item=new User();
 }
 }
 get f()
